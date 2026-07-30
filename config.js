b646bcc802eb0d559fdd3c055ab753bf0ca1a1c1
praveenkumar2005123-glitch <praveenkumar2005123@gmail.com>
2026-07-30 11:02:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
-// Paste your Firebase Web App configuration below.
-// Firebase Console → Project Settings → Your apps → Web app → SDK setup and configuration.
 window.firebaseConfig = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "PASTE_PROJECT.firebaseapp.com",
-  databaseURL: "https://PASTE_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "PASTE_PROJECT",
-  storageBucket: "PASTE_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyDptvjm7TAUs0m35M9yngEXpHC9hOJ4suI",
+  authDomain: "the-buzzer-a4d73.firebaseapp.com",
+  databaseURL: "https://the-buzzer-a4d73-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "the-buzzer-a4d73",
+  storageBucket: "the-buzzer-a4d73.firebasestorage.app",
+  messagingSenderId: "777094158178",
+  appId: "1:777094158178:web:ae764644f1a022febbfe3b",
+  measurementId: "G-6T3PDKEVNR"
 };
